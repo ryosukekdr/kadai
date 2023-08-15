abf09c96a84972f774deaeb8d0dfe2e7d55b7d06
ryosuke <ryosukekdr@yahoo.co.jp>
2023-08-15 04:51:40 +0000

    【フロントエンド】07_課題

---
+++ 07.js
@@ -0,0 +1,13 @@
+let fruits = {apple:'りんご', strawberry:'いちご', grape:'ぶどう', lemon:'れもん'};
+
+console.log(fruits['grape']);
+
+
+for (key in fruits) {
+    console.log(key);
+}
+
+
+for (key in fruits) {
+    console.log(fruits[key]);
+}
